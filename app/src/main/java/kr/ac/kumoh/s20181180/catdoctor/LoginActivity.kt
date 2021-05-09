47549e565685434796ff9f788c39cda18b34a9aa
choiyeonjii <yeonjiyaaa@naver.com>
2021-05-10 00:05:58 +0900

    google login

---
+++ app/src/main/java/kr/ac/kumoh/s20181180/catdoctor/LoginActivity.kt
@@ -0,0 +1,132 @@
+package kr.ac.kumoh.s20181180.catdoctor
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.util.Utility
+import com.kakao.sdk.user.UserApiClient
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        // 로그인 공통 callback 구성
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient=GoogleSignIn.getClient(this,gso)
+        auth = Firebase.auth
+
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e(KAKAO_TAG, "로그인 실패", error)
+            }
+            else if (token != null) {
+                Log.v(KAKAO_TAG, "로그인 성공")
+                startMainActivity()
+            }
+        }
+        google_login_btn.setOnClickListener {
+            signIn()
+        }
+        findViewById<ImageView>(R.id.kakao_login_btn).setOnClickListener {
+
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(GOOGLE_TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(GOOGLE_TAG, "Google sign in failed", e)
+            }
+        }
+    }
+    // [END onactivityresult]
+
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(GOOGLE_TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(GOOGLE_TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+    // [END auth_with_google]
+
+    // [START signin]
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // [END signin]
+
+    private fun updateUI(user: FirebaseUser?) {
+        startMainActivity()
+    }
+
+    companion object {
+        private const val GOOGLE_TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+        private const val KAKAO_TAG = "kakaoLoginActivity"
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+
+}
--- app/src/main/java/kr/ac/kumoh/s20181180/catdoctor/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181180/catdoctor/MainActivity.kt
@@ -4,16 +4,15 @@ package kr.ac.kumoh.s20181180.catdoctor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
+import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import com.kakao.sdk.user.UserApiClient
-
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -25,36 +24,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         mQueue = Volley.newRequestQueue(this)
 
-        requestGundam()
-        kakao_logout_btn.setOnClickListener {
-            // 로그아웃
-            UserApiClient.instance.logout { error ->
-                if (error != null) {
-                    Log.e("TAG","로그아웃 실패. SDK에서 토큰 삭제됨", error)
-                }
-                else {
-                    Toast.makeText(this, "로그아웃 성공. SDK에서 토큰 삭제됨", Toast.LENGTH_LONG).show()
-                    Log.i("TAG", "로그아웃 성공. SDK에서 토큰 삭제됨")
-                    finish()
-                }
-            }
-        }
-        kakao_signout_btn.setOnClickListener{
-            // 연결 끊기
-            UserApiClient.instance.unlink { error ->
-                if (error != null) {
-                    Log.e("TAG", "연결 끊기 실패", error)
-                }
-                else {
-                    Toast.makeText(this, "연결 끊기 성공. SDK에서 토큰 삭제됨", Toast.LENGTH_LONG).show()
-                    Log.i("TAG", "연결 끊기 성공. SDK에서 토큰 삭제 됨")
-                    finish()
-                }
-            }
-        }
+        //requestGundam()
     }
 
     override fun onStop() {
@@ -64,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestGundam() {
         // NOTE: 서버 주소는 본인의 서버 IP 사용할 것
-        val url = "http://192.168.0.6:8080/symptom"
+        val url = "http://192.168.0.15:8080/symptom"
 
         val request = JsonArrayRequest(
                 Request.Method.GET,
@@ -82,5 +54,4 @@ class MainActivity : AppCompatActivity() {
         request.tag = QUEUE_TAG
         mQueue.add(request)
     }
-
 }
